77f9d64a2ac29547b73690b7edfc0638319ce04b
abackup <40161397+abackup@users.noreply.github.com>
2024-07-15 12:29:31 +0800

    Update RemoteLight8266.ino

--- RemoteLight8266/RemoteLight8266.ino
+++ RemoteLight8266/RemoteLight8266.ino
@@ -30,9 +30,9 @@
 #error ERROR: THIS SKETCH IS DESIGNED FOR ESP8266 MICROCONTROLLERS!
 #endif
 
-// This is the sketch for a remote ESP8266 device, which cannot run HomeSpan or SpanPoint.  The sketch therefore directly uses ESP-NOW functions to receive and transmit data to the Main Device (the CENTRAL HUB)
+// 这是远程 ESP8266 设备的草图，无法运行 HomeSpan 或 SpanPoint。因此，该草图直接使用 ESP-NOW 函数接收和传输数据到主设备（中央集线器）
 
-uint8_t MAIN_DEVICE_MAC[6]={0xAC,0x67,0xB2,0x77,0x42,0x21};       // use the corresponding "Local MAC Address" listed under SpanPoint section when typing 'i' into the Serial Monitor ON THE CENTRAL HUB SKETCH
+uint8_t MAIN_DEVICE_MAC[6]={0xAC,0x67,0xB2,0x77,0x42,0x21};       // 在中央集线器草图上的串行监视器中输入“i”时，使用 SpanPoint 部分下列出的相应“本地 MAC 地址”
 #define LED_PIN               0
 #define CALIBRATION_PERIOD    20
 #define INTERNAL_ID           8266
@@ -49,7 +49,7 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 
 void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len) {
   Serial.printf("Packet Receieved from %02X:%02X:%02X:%02X:%02X:%02X: %d bytes\n",mac_addr[0],mac_addr[1],mac_addr[2],mac_addr[3],mac_addr[4],mac_addr[5],len);
-  digitalWrite(LED_PIN,!incomingData[0]);     // the only byte we should receive from the Maon Device is a boolean indicating if LED should be on or off (note reverse LED: HIGH=OFF, LOW=ON)
+  digitalWrite(LED_PIN,!incomingData[0]);     // 我们应该从 Maon 设备接收的唯一字节是一个布尔值，表示 LED 应该打开还是关闭（注意反向 LED：HIGH=OFF，LOW=ON）
 }
 
 void setup() {
@@ -61,35 +61,34 @@ void setup() {
   Serial.printf("NOTE: This MAC Address must be entered into the corresponding SpanPoint() call of the MAIN Device.\n\n");
 
   WiFi.mode(WIFI_STA);
-  wifi_set_channel(6);              // Here we need to manually set the channel to match whatever the Central Hub reports when typing 'i' on the Serial Monitor
+  wifi_set_channel(6);              // 在这里，我们需要手动设置通道，以匹配在串行监视器上输入“i”时中央集线器报告的内容
 
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW.  HALTING!");
     while(1);
   }  
 
-  uint8_t hash[32];                 // create space to store as 32-byte hash code
-  char password[]="HomeSpan";       // specify the password
+  uint8_t hash[32];                 // 创建空间来存储 32 字节哈希码
+  char password[]="HomeSpan";       // 指定密码
   
-  experimental::crypto::SHA256::hash(password,strlen(password),hash);     // create the hash code to be used further below
+  experimental::crypto::SHA256::hash(password,strlen(password),hash);     // 创建下面要使用的哈希码
 
-  esp_now_register_send_cb(OnDataSent);                      // register the callback for sending data
-  esp_now_register_recv_cb(OnDataRecv);                      // register the callback for receiving data
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);                 // set the role of this device to be a controller (i.e. it sends data to the ESP32)
+  esp_now_register_send_cb(OnDataSent);                      // 注册发送数据的回调
+  esp_now_register_recv_cb(OnDataRecv);                      // 注册接收数据的回调
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);                 // 将此设备的角色设置为控制器（即，它将数据发送到 ESP32）
 
-  esp_now_set_kok(hash+16,16);                                    // next we set the PMK.  For some reason this is called KOK on the ESP8266.  Note you must set the PMK BEFORE adding any peers
+  esp_now_set_kok(hash+16,16);                                    // 接下来我们设置 PMK。出于某种原因，这在 ESP8266 上称为 KOK。请注意，您必须在添加任何对等点之前设置 PMK
         
-  esp_now_add_peer(MAIN_DEVICE_MAC, ESP_NOW_ROLE_COMBO, 0, hash, 16);    // now we add in the peer, set its role, and specify the LMK
+  esp_now_add_peer(MAIN_DEVICE_MAC, ESP_NOW_ROLE_COMBO, 0, hash, 16);    // 现在我们添加对等体，设置其角色，并指定 LMK
 
   pinMode(LED_PIN,OUTPUT);
-  digitalWrite(LED_PIN,1);                                   // the built-in LED on the Adafruit 8266 Featherboard is reverse wired - HIGH=OFF, LOW=ON
-}
+  digitalWrite(LED_PIN,1);                                   // Adafruit 8266 Featherboard 上的内置 LED 是反向接线的 - HIGH=OFF，LOW=ON
 
 void loop() {
 
   if(millis()>calibrateTime){
     int id=INTERNAL_ID;
-    esp_now_send(MAIN_DEVICE_MAC, (uint8_t *)&id, sizeof(id));     // Send the Data to the Main Device!
+    esp_now_send(MAIN_DEVICE_MAC, (uint8_t *)&id, sizeof(id));     // 将数据发送到主设备！
     calibrateTime=millis()+CALIBRATION_PERIOD*1000;
   }
 }
